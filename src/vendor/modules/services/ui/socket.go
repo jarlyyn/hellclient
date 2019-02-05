302a821833851305989348b6a8ffb771725d0235
Jarlyyn <jarlyyn@qq.com>
2019-02-06 01:16:01 +0800

    update

--- src/vendor/modules/services/ui/socket.go
+++ src/vendor/modules/services/ui/socket.go
@@ -2,115 +2,71 @@ package ui
 
 import (
 	"fmt"
-	"io"
 	"net/http"
 	"sync"
 
-	"github.com/gorilla/websocket"
+	"github.com/jarlyyn/herb-go-experimental/websocketmanager"
+	"github.com/jarlyyn/herb-go-experimental/websocketmanager/websocket"
 )
 
-var Current *Conn
+var Current *websocketmanager.RegisteredConn
 var Locker sync.RWMutex
-var upgrader = websocket.Upgrader{} // use default options
+var socketmamager = websocketmanager.New()
 
-type Conn struct {
-	*websocket.Conn
-	closed bool
-	Lock   sync.Mutex
+func OnMsg(msg *websocketmanager.ConnMessage) {
+	fmt.Println(string(msg.Message))
 }
-
-func (c *Conn) Close() error {
-	c.Lock.Lock()
-	defer c.Lock.Unlock()
-	c.closed = true
-	return c.Conn.Close()
-}
-
-func (c *Conn) isClosed() bool {
-	c.Lock.Lock()
-	defer c.Lock.Unlock()
-	return c.closed
-
-}
-
-type Msg struct {
-	Conn *Conn
-	Type int
-	Data []byte
-}
-
-func NewMsg() *Msg {
-	return &Msg{}
-}
-func NewConn() *Conn {
-	return &Conn{}
-}
-
-func OnMsg(msg *Msg) {
-	fmt.Println(msg)
-}
-func Send(msgType int, data []byte) error {
+func Send(data []byte) error {
 	Locker.Lock()
 	c := Current
 	Locker.Unlock()
 	if c == nil {
 		return nil
 	}
-	return c.WriteMessage(msgType, data)
+	return c.Send(data)
 }
 
-var OnErr func(err error)
+var OnErr func(err *websocketmanager.ConnError)
 
 func init() {
-	OnErr = func(err error) {
-		fmt.Println(err)
+	OnErr = func(err *websocketmanager.ConnError) {
+		fmt.Println(*err)
 	}
 }
 
 var Enter = func(w http.ResponseWriter, r *http.Request) error {
 
-	c, err := upgrader.Upgrade(w, r, nil)
+	wc, err := websocket.Upgrade(w, r, websocket.MsgTypeText)
 	if err != nil {
 		return err
 	}
-	conn := NewConn()
-	conn.Conn = c
+	c, err := socketmamager.Register(wc)
+	if err != nil {
+		return err
+	}
 	Locker.Lock()
 	old := Current
-	Current = conn
+	Current = c
 	Locker.Unlock()
 	go func() {
-		defer func() {
-
-		}()
-		defer func() {
-			recover()
-		}()
-		for {
-			mt, msg, err := c.ReadMessage()
-			if err == io.EOF {
-				break
-			}
-			if err != nil {
-				if conn.isClosed() {
-					break
-				}
-				if websocket.IsUnexpectedCloseError(err) {
-					conn.Close()
-					break
-				}
-				OnErr(err)
-				continue
-			}
-			m := NewMsg()
-			m.Conn = conn
-			m.Type = mt
-			m.Data = msg
-			OnMsg(m)
-		}
+		<-c.C()
 	}()
 	if old != nil {
 		old.Close()
 	}
 	return nil
+}
+
+func Listen() {
+	for {
+		select {
+		case m := <-socketmamager.ConnMessages:
+			OnMsg(m)
+		case err := <-socketmamager.ConnErrors:
+			OnErr(err)
+		}
+	}
+}
+func init() {
+	go Listen()
 }
