9740e903cda55e03b84d406411b18a6cf221b6a9
Jarlyyn <jarlyyn@qq.com>
2019-02-18 00:12:55 +0800

    update

--- src/vendor/modules/services/ui/forms/savetriggerform.go
+++ src/vendor/modules/services/ui/forms/savetriggerform.go
@@ -1,39 +1,47 @@
-package  forms
+package forms
 
 import (
-	"github.com/herb-go/herb/model/formdata"
-    "net/http"
-    "modules/messages"
-)
+	"net/http"
 
+	"github.com/herb-go/herb/model/formdata"
+)
 
 //SaveTriggerFormFieldLabels form field labels map.
 var SaveTriggerFormFieldLabels = map[string]string{
-	"Field1":  "Field 1",
-	"Field2":    "Field 2",
+	"Pattern":  "匹配项",
+	"IsRegExp": "正则",
+	"Enabled":  "有效",
+	"Commnd":   "命令",
+	"Priority": "优先度",
+	"Finally":  "不再继续匹配",
 }
 
 //SaveTriggerForm form struct for save trigger
 type SaveTriggerForm struct {
 	formdata.Form
-	Field1  *string
-	Field2    *string
+	Pattern  string
+	IsRegExp bool
+	Enabled  bool
+	Commnd   string
+	Priority int
+	Finally  bool
 }
 
 //SaveTriggerFormID form id of form save trigger
 const SaveTriggerFormID = "formservices.savetrigger"
 
 //NewSaveTriggerForm create new save trigger form
-func NewSaveTriggerForm() *SaveTriggerForm{
-	form:=&SaveTriggerForm{}
+func NewSaveTriggerForm() *SaveTriggerForm {
+	form := &SaveTriggerForm{}
 	form.SetModelID(SaveTriggerFormID)
 	form.SetFieldLabels(SaveTriggerFormFieldLabels)
 	return form
 }
+
 //Validate Validate form and return any error if raised.
 func (f *SaveTriggerForm) Validate() error {
-    f.ValidateFieldf(f.Field1 != nil, "Field1", messages.MsgFormFieldRequired) 
-    f.ValidateFieldf(f.Field2 != nil, "Field2", messages.MsgFormFieldRequired) 
+	f.ValidateFieldf(f.Pattern != "", "Pattern", "匹配项不能为空")
+
 	if !f.HasError() {
 	}
 	return nil
