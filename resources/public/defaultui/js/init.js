b7f9abe5c0e02aa34f54eb5e359bb3fbebf8cba2
Jarlyyn <jarlyyn@qq.com>
2019-02-24 20:20:03 +0800

    update

--- resources/public/defaultui/js/init.js
+++ resources/public/defaultui/js/init.js
@@ -4,12 +4,18 @@ requirejs.config({
         "vue":"https://cdn.jsdelivr.net/npm/vue@2.5.21/dist/vue",
         "ELEMENT":"https://unpkg.com/element-ui/lib/index",
         "html-top":"/public/defaultui/block/top.html",
+        "html-triggers":"/public/defaultui/block/triggers.html",
+        "html-creategameform":"/public/defaultui/block/creategameform.html",
         "main":"/public/defaultui/js/main"
     },
 });
 define(function (require) {
     var htmltop=require("text!html-top");
     document.getElementById("top").innerHTML=htmltop;
+    var htmltriggers=require("text!html-triggers");
+    document.getElementById("triggers").innerHTML=htmltriggers;
+    var htmlcreategameform=require("text!html-creategameform");
+    document.getElementById("creategameform").innerHTML=htmlcreategameform;
 
     require(["main"],function(main){
         main()
