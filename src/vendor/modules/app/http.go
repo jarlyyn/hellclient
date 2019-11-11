e5ccfeca57a70db1ba14bee539f46a16dd91b2e7
Jarlyyn <jarlyyn@qq.com>
2019-11-12 00:34:07 +0800

    update

--- src/vendor/modules/app/http.go
+++ src/vendor/modules/app/http.go
@@ -3,21 +3,21 @@ package app
 import (
 	forwarded "github.com/herb-go/herb/middleware/forwarded"
 	"github.com/herb-go/herb/middleware/misc"
+	"github.com/herb-go/herb/service/httpservice"
 	"github.com/herb-go/util"
 	"github.com/herb-go/util/config"
 	"github.com/herb-go/util/config/tomlconfig"
-	"github.com/herb-go/util/httpserver"
 )
 
 type HTTPConfig struct {
 	Forwarded forwarded.Middleware
-	Config    httpserver.Config
+	Config    httpservice.Config
 	Headers   misc.Headers
 }
 
 var HTTP = &HTTPConfig{
 	Forwarded: forwarded.Middleware{},
-	Config:    httpserver.Config{},
+	Config:    httpservice.Config{},
 	Headers:   misc.Headers{},
 }
 
