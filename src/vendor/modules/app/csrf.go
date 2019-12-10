e69fcffd28d557a738b9a345adf7d3c648896466
jarlyyn <jarlyyn@qq.com>
2019-12-10 20:25:15 +0800

    update

--- src/vendor/modules/app/csrf.go
+++ src/vendor/modules/app/csrf.go
@@ -2,7 +2,7 @@ package app
 
 import (
 	"github.com/herb-go/herb/middleware/csrf"
-	"github.com/herb-go/herbconfig/configuration"
+	"github.com/herb-go/herbconfig/source"
 	"github.com/herb-go/util"
 	"github.com/herb-go/util/config"
 	"github.com/herb-go/util/config/tomlconfig"
@@ -11,7 +11,7 @@ import (
 var Csrf = csrf.Config{}
 
 func init() {
-	config.RegisterLoader(util.ConfigFile("/csrf.toml"), func(configpath configuration.Configuration) {
+	config.RegisterLoader(util.ConfigFile("/csrf.toml"), func(configpath source.Source) {
 		util.Must(tomlconfig.Load(configpath, &Csrf))
 	})
 }
