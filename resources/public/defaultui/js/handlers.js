babfbeaa272cd4ebf981b6e6aa3d8dacb513ee3f
jarlyyn <jarlyyn@qq.com>
2021-06-03 00:33:21 +0800

    update

---
+++ resources/public/defaultui/js/handlers.js
@@ -0,0 +1,72 @@
+define(function (require) {
+    var app=require("/public/defaultui/js/app.js")
+    var vm=require("/public/defaultui/js/vm.js")
+    var handlers=app.handlers;
+    var send=app.send;
+   
+
+handlers.current=function(data){
+    vm.current=data
+    vm.currenttab=data
+}
+handlers.line=function(data){
+    var lines=vm.lines
+    lines.push(data)
+    if (lines.length>500){
+        lines.shift()
+    }else{
+        setTimeout(function(){
+        body.scrollTo(body.offsetLeft,body.offsetHeight)
+        },0)
+    }
+    vm.lines=lines
+}
+handlers.prompt=function(data){
+    vm.prompt=data
+}
+handlers.clients=function(data){
+    vm.clients=data
+    data.forEach(function(client) {
+        vm.running[client.ID]=client.Running
+    })
+}
+handlers.lines=function(data){
+    var lines=[]
+    data.forEach(function(element) {
+        lines.push(element)
+    if (lines.length>500){
+        lines.shift()
+    }
+    })
+    vm.lines=lines
+    setTimeout(function(){
+        body.scrollTo(body.offsetLeft,body.offsetHeight)
+        },0)
+}
+handlers.connected=function(data){
+    vm.running[data]=true
+}
+handlers.disconnected=function(data){
+    vm.running[data]=false
+}
+handlers.createFail=function(data){
+    vm.createFail=data
+}
+handlers.createSuccess=function(data){
+    vm.gameCreateFormVisible=false;
+    send("change",data)
+}
+handlers.triggers=function(data){
+    vm.triggers=data
+}
+handlers.triggerFail=function(data){
+    vm.saveTriggerFail=data
+}
+handlers.triggerSuccess=function(data){
+    vm.triggerSaveFormVisible=false;
+}
+handlers.allLines=function(data){
+    vm.allLines=data
+}
+
+})
---
+++ resources/public/defaultui/js/init.js
@@ -0,0 +1,25 @@
+requirejs.config({
+    "paths": {
+        "text":"https://cdn.staticfile.org/require-text/2.0.12/text.min",
+        "vue":"https://cdn.jsdelivr.net/npm/vue@2.5.21/dist/vue",
+        "ELEMENT":"https://unpkg.com/element-ui/lib/index",
+        "html-top":"/public/defaultui/block/top.html",
+        "html-triggers":"/public/defaultui/block/triggers.html",
+        "html-creategameform":"/public/defaultui/block/creategameform.html",
+        "html-alllines":"/public/defaultui/block/alllines.html",
+        "main":"/public/defaultui/js/main"
+    },
+});
+define(function (require) {
+    var htmltop=require("text!html-top");
+    document.getElementById("top").innerHTML=htmltop;
+    var htmltriggers=require("text!html-triggers");
+    document.getElementById("triggers").innerHTML=htmltriggers;
+    var htmlcreategameform=require("text!html-creategameform");
+    document.getElementById("creategameform").innerHTML=htmlcreategameform;
+    var htmlalllines=require("text!html-alllines");
+    document.getElementById("alllines").innerHTML=htmlalllines;
+
+    require(["main"],function(main){
+    });
+})
