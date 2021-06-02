710f1c58e2ee15e40b68ac4d12fddb3c6e339ce4
jarlyyn <jarlyyn@qq.com>
2021-06-03 01:05:32 +0800

    update

--- src/vendor/modules/routers/ui.go
+++ src/vendor/modules/routers/ui.go
@@ -1,9 +1,11 @@
 package routers
 
 import (
+	"github.com/herb-go/herb/file/simplehttpserver"
 	"github.com/herb-go/herb/middleware"
 	"github.com/herb-go/herb/middleware/router"
 	"github.com/herb-go/herb/middleware/router/httprouter"
+	"github.com/herb-go/util"
 )
 
 //UIMiddlewares middlewares which should be used on router.
@@ -14,6 +16,9 @@ var UIMiddlewares = func() middleware.Middlewares {
 //RouterUIFactory ui router factory.
 var RouterUIFactory = router.NewFactory(func() router.Router {
 	var Router = httprouter.New()
+	Router.StripPrefix("/public").
+		HandleFunc(simplehttpserver.ServeFolder(util.Resources("public")))
+	Router.GET("/").HandleFunc(simplehttpserver.ServeFile(util.Resources("defaultui", "index.html")))
 	//Put your router configure code here
 	return Router
 })
