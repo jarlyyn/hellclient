5ff390ec86217dd4659331b69751c942e038ef26
jarlyyn <jarlyyn@qq.com>
2021-06-27 02:08:31 +0800

    update

--- src/vendor/modules/world/clientinfo.go
+++ src/vendor/modules/world/clientinfo.go
@@ -1,5 +1,9 @@
 package world
 
+import (
+	"github.com/herb-go/herbplugin"
+)
+
 type ClientInfo struct {
 	ID       string
 	ReadyAt  int64
@@ -23,4 +27,24 @@ func (info ClientInfos) Less(i, j int) bool {
 // Swap swaps the elements with indexes i and j.
 func (info ClientInfos) Swap(i, j int) {
 	info[i], info[j] = info[j], info[i]
+}
+
+type WorldData struct {
+	Host        string
+	Port        string
+	Charset     string
+	QueueDelay  int
+	Params      map[string]string
+	Permissions []string
+	ScriptID    string
+	Trusted     herbplugin.Trusted
+	Triggers    []*Trigger
+	Timers      []*Timer
+	Alias       []*Alias
+}
+
+func NewWorldData() *WorldData {
+	return &WorldData{
+		Params: map[string]string{},
+	}
 }
