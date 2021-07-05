52cc75cb2e96c10ab98b16486a056f275021ecb5
jarlyyn <jarlyyn@qq.com>
2021-07-06 00:42:02 +0800

    update

---
+++ lib/pcre/pcre.md
@@ -0,0 +1,5 @@
+# Pcre 正则dll
+用于实现lua.rex的正则库
+
+下载自
+http://win-builds.org/1.5.0/packages/windows_64/pcre-8.36-1-x86_64-w64-mingw32.txz
--- src/build/build-linux.sh
+++ src/build/build-linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
-go build  -ldflags '-extldflags "-static"' --trimpath -o ../../bin/hellclient ../
+export lib=`cd $(dirname $0)/../../lib/pcre; pwd`
+CGO_LDFLAGS="-lpcre -static" CGO_ENABLED=1 go build  --trimpath -o ../../bin/app ../
 go build  --trimpath -o ../../bin/mclconvertor ../mclconvertor
 
