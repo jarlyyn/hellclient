9d5c50b606b451373c615485e0b18aa4cf7c0b53
jarlyyn <jarlyyn@qq.com>
2021-12-26 14:36:49 +0800

    update

--- src/modules/intro/init.go
+++ src/modules/intro/init.go
@@ -3,7 +3,6 @@ package intro
 import (
 	"fmt"
 	"hellclient/modules/app"
-	"hellclient/modules/version"
 
 	"github.com/herb-go/util"
 )
@@ -15,7 +14,6 @@ func init() {
 	util.StageFinish.RegisterModule(ModuleName, func() {
 		//Init registered initator which registered by RegisterInitiator
 		//util.RegisterInitiator(ModuleName, "func", func(){})
-		fmt.Printf("Hellclient v%s\n", version.Version)
 		fmt.Printf("Listening http on %s\n", app.System.Addr)
 		util.InitOrderByName(ModuleName)
 	})
