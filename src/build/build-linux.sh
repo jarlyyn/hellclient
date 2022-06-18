64fd8736eec62f675e4f784643b5305645a38eba
Jarlyyn <jarlyyn@qq.com>
2022-06-19 03:10:49 +0800

    update

--- src/build/build-linux.sh
+++ src/build/build-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export lib=`cd $(dirname $0)/../../lib/pcre; pwd`
-# CGO_LDFLAGS="-lpcre -static" CGO_ENABLED=1 go build  --trimpath -o ../../bin/hellclient ../
-CGO_LDFLAGS="-lpcre" CGO_ENABLED=1 go build  --trimpath -o ../../bin/hellclient ../
+CGO_LDFLAGS="-lpcre -static" CGO_ENABLED=1 go build  --trimpath -o ../../bin/hellclient ../
+# CGO_LDFLAGS="-lpcre" CGO_ENABLED=1 go build  --trimpath -o ../../bin/hellclient ../
 go build  --trimpath -o ../../bin/mclconvertor ../mclconvertor
 
