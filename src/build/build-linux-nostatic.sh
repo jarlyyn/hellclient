8b71dc241c7d2dd5d99f8e43b093fedc3cece867
Jarlyyn <jarlyyn@qq.com>
2022-07-07 01:10:02 +0800

    update

---
+++ src/build/build-linux-nostatic.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export lib=`cd $(dirname $0)/../../lib/pcre; pwd`
+go build  -tags nopcre --trimpath -o ../../bin/hellclient ../
+# CGO_LDFLAGS="-lpcre" CGO_ENABLED=1 go build  --trimpath -o ../../bin/hellclient ../
+go build  --trimpath -o ../../bin/mclconvertor ../mclconvertor
+
