48060d609a2d127a82795d99910b3ac12542311c
Jarlyyn <jarlyyn@qq.com>
2023-09-21 22:59:24 +0800

    update

--- src/build/build-win.sh
+++ src/build/build-win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export lib=`cd $(dirname $0)/../../lib/pcre; pwd`
-CGO_CFLAGS="-I$lib" CGO_LDFLAGS="-L$lib -lpcre" CC=x86_64-w64-mingw32-gcc GOOS=windows GOARCH=amd64 CGO_ENABLED=1 go build  -tags nopcre -a --trimpath -o ../../bin/hellclient.exe ../
+CGO_CFLAGS="-I$lib" CGO_LDFLAGS="-L$lib -lpcre" CC=x86_64-w64-mingw32-gcc GOOS=windows GOARCH=amd64 CGO_ENABLED=1 go build   --trimpath -o ../../bin/hellclient.exe ../
 CC=x86_64-w64-mingw32-gcc GOOS=windows GOARCH=amd64 go build --trimpath -o ../../bin/mclconvertor.exe ../mclconvertor
