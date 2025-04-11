888fafbe11981a8f819cb14bed05608ef4262677
Jarlyyn <jarlyyn@qq.com>
2025-04-11 22:01:21 +0800

    update

--- src/modules/world/component/script/luaengine/nopcre.go
+++ src/modules/world/component/script/luaengine/nopcre.go
@@ -1,4 +1,5 @@
-// +build nopcre
+//go:build !withpcre
+// +build !withpcre
 
 package luaengine
 
--- src/modules/world/component/script/luaengine/rexmodule.go
+++ src/modules/world/component/script/luaengine/rexmodule.go
@@ -1,5 +1,5 @@
-//go:build !nopcre
-// +build !nopcre
+//go:build withpcre
+// +build withpcre
 
 package luaengine
 
